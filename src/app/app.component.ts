b9614766d4598fef306a185dd44cb8be81d9332f
kjbrawner <kjbrawner22@gmail.com>
2017-10-16 11:45:05 -0500

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,59 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { FavoritesPage } from '../pages/favorites/favorites'
+import { InfoPage } from '../pages/info/info'
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  pages: Array<{title: string, component: any}>;
+  data: any;
+
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public http: HttpClient) {
+    this.initializeApp();
+    this.APIDataRetrieval();
+
+    this.pages = [
+      {title: 'Home', component: HomePage},
+      {title: 'Favorites', component: FavoritesPage},
+      {title: 'Info', component: InfoPage}
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
-}
 
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  APIDataRetrieval() {
+    this.http.get("https:api.coinmarketcap.com/v1/ticker/").subscribe(
+      data => {
+        //JSON Response
+        this.data = data;
+        console.log(data);
+      },
+      err => {
+        console.log("error");
+      }
+    );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { InfoPage } from '../pages/info/info'
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FavoritesPage,
+    InfoPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FavoritesPage,
+    InfoPage
   ],
   providers: [
     StatusBar,
